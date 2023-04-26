cac2557e97323bdb596d2f737a56d54d93a8a709
ferJen <ferjen.coder@gmail.com>
2023-04-26 15:01:10 -0300

    chore: Aesthetics mainly

--- src/desafios-entregables/01-desafio.js
+++ src/desafios-entregables/01-desafio.js
@@ -26,7 +26,8 @@ export default class ProductManager {
         );
 
         if ( productIndex >= 0 ) {
-            throw new Error( "Msg from ferJen: Product already exists" )
+            console.error( "Msg from ferJen: Product already exists" );
+            return false;
         };
 
         const product = {
@@ -53,6 +54,7 @@ export default class ProductManager {
         const storedProduct = this.products.find( product => product.id === id );
 
         if ( !storedProduct ) {
+            console.error( "Msg from ferJen: Product not found" );
             return new Error( "Msg from ferJen: Product not found" );
         }
 
@@ -60,41 +62,40 @@ export default class ProductManager {
     }
 };
 
+const productManager = new ProductManager();
+productManager.addProduct(
+    'producto prueba',
+    'Este es un producto prueba',
+    200,
+    'Sin imagen',
+    'abc123',
+    25
+);
+productManager.addProduct(
+    'producto prueba',
+    'Este es un producto prueba',
+    200,
+    'Sin imagen',
+    'abc123',
+    25
+);
+productManager.addProduct(
+    'producto prueba uno',
+    'Este es un producto prueba uno',
+    205,
+    'Sin imagen uno',
+    'abc123 uno',
+    35
+);
+productManager.addProduct(
+    'producto prueba dos',
+    'Este es un producto prueba dos',
+    210,
+    'Sin imagen dos',
+    'abc123 dos',
+    45
+);
 
-// const manager = new ProductManager();
-// manager.addProduct(
-//     'producto prueba',
-//     'Este es un producto prueba',
-//     200,
-//     'Sin imagen',
-//     'abc123',
-//     25
-// );
-// manager.addProduct(
-//     'producto prueba',
-//     'Este es un producto prueba',
-//     200,
-//     'Sin imagen',
-//     'abc123',
-//     25
-// );
-// manager.addProduct(
-//     'producto prueba uno',
-//     'Este es un producto prueba uno',
-//     205,
-//     'Sin imagen uno',
-//     'abc123 uno',
-//     35
-// );
-// manager.addProduct(
-//     'producto prueba dos',
-//     'Este es un producto prueba dos',
-//     210,
-//     'Sin imagen dos',
-//     'abc123 dos',
-//     45
-// );
-
-// console.log( 'Product found =>', manager.getProductById( 1 ) );
-// console.log( manager.getProductById( 85 ) );
-// console.log( 'Products in store =>', manager.getProducts() );
+console.log( 'Product found =>', productManager.getProductById( 1 ) );
+console.log( productManager.getProductById( 85 ) );
+console.log( 'Products in store =>', productManager.getProducts() );
