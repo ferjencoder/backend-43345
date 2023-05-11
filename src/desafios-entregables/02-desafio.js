0c507e98338baa50a031f22c6440cd2bc7dee604
ferJen <ferjen.coder@gmail.com>
2023-05-10 21:18:25 -0300

    chore: Updated 02-desafio

--- src/desafios-entregables/02-desafio.js
+++ src/desafios-entregables/02-desafio.js
@@ -13,12 +13,11 @@ export default class ProductManager {
     }
 
     getProducts = async () => {
-        if ( fs.existsSync() ) {
+        if ( fs.existsSync( path ) ) {
             const data = await fs.promises.readFile( path, 'utf-8' );
 
             const productsJSON = JSON.parse( data );
 
-            // comment
             this.products = productsJSON;
 
             return productsJSON
@@ -39,7 +38,7 @@ export default class ProductManager {
             console.warn( `Missing product info (title, description, price, thumbnail, code, stock)` );
         }
 
-        const productsJSON = await this.getProducts();
+        const productsJSON = await this.getProducts( path );
 
         const productIndex = productsJSON.findIndex(
             product => product.code === code
@@ -93,8 +92,12 @@ export default class ProductManager {
         code,
         stock
     ) => {
+
+
+        // console.log( this.getProducts() );
+
         const storedProductIndex = this.products.findIndex(
-            ( product ) => product.code === code
+            ( product ) => product.id === id
         );
 
         console.log( storedProductIndex );
@@ -116,6 +119,7 @@ export default class ProductManager {
         };
 
         this.products[ storedProductIndex ] = updatedProduct;
+        console.log( this.products );
 
         await fs.promises.writeFile(
             path,
@@ -124,54 +128,71 @@ export default class ProductManager {
 
         console.log( 'updatedProduct', updatedProduct );
 
-
         return updatedProduct;
     };
 };
 
 const productManager = new ProductManager();
-productManager.addProduct(
-    'producto prueba',
-    'Este es un producto prueba',
-    200,
-    'Sin imagen',
-    '2323223',
-    25
-);
-productManager.addProduct(
-    'producto prueba',
-    'Este es un producto prueba',
-    200,
-    'Sin imagen',
-    'abc123',
-    25
-);
-productManager.addProduct(
-    'producto prueba uno',
-    'Este es un producto prueba uno',
-    205,
-    'Sin imagen uno',
-    'abc123 uno',
-    35
-);
-productManager.addProduct(
-    'producto prueba dos',
-    'Este es un producto prueba dos',
-    210,
-    'Sin imagen dos',
-    'abc123 dos',
-    45
+
+const newProduct = await productManager.addProduct(
+    'Product A',
+    'Description of Product A',
+    100.0,
+    'https://example.com/product-a-thumbnail.jpg',
+    'PA001',
+    10
 );
 
-productManager.updateProduct(
-    'producto prueba',
-    'Este es un producto pruebaaaaa',
-    8550,
-    'Sin imagen',
-    '2323223',
-    1225
+const updatedProduct = await productManager.updateProduct(
+    1,
+    'Product A',
+    'New description of Product A',
+    1200.0,
+    'https://example.com/product-a-thumbnail-v2.jpg',
+    'PA001',
+    155
 );
+
+productManager.getProducts().then( ( products ) => {
+    console.log( 'Products in store =>', products );
+} );
+
+
+async function logProducts () {
+    const products = await productManager.getProducts();
+    console.log( 'Products in store =>', products );
+}
+
+logProducts();
+
+
+console.log( 'Product updated:', updatedProduct );
+
 
 console.log( 'Product found =>', productManager.getProductById( 1 ) );
 console.log( 'Products in store =>', productManager.getProducts() );
-console.log( productManager.getProductById( 85 ) );
+//console.log( productManager.getProductById( 85 ) );
+// productManager.addProduct(
+//     'producto prueba',
+//     'Este es un producto prueba',
+//     200,
+//     'Sin imagen',
+//     'abc123',
+//     25
+// );
+// productManager.addProduct(
+//     'producto prueba uno',
+//     'Este es un producto prueba uno',
+//     205,
+//     'Sin imagen uno',
+//     'abc123 uno',
+//     35
+// );
+// productManager.addProduct(
+//     'producto prueba dos',
+//     'Este es un producto prueba dos',
+//     210,
+//     'Sin imagen dos',
+//     'abc123 dos',
+//     45
+// );
